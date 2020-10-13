4f2fb18ec2eadc6d8eb137b77044260e80e762c0
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-13 19:29:21 +0800

    modified sign-in screen so inputs background is 10% opaque white

--- src/screens/sign-in/sign-in.styles.js
+++ src/screens/sign-in/sign-in.styles.js
@@ -4,11 +4,15 @@ export default StyleSheet.create({
   container: { flex: 1 },
   logo: { flex: 3, alignItems: 'center', justifyContent: 'center' },
   form: { flex: 6 },
+  textInput: {
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    color: 'white'
+  },
   loginButton: { marginTop: 6 },
   forgotPassword: { padding: 15, marginTop: 10 },
-  forgotPasswordText: { color: 'white', alignSelf: 'center' },
+  forgotPasswordText: { alignSelf: 'center' },
   signUp: { flex: 2, alignItems: 'center' },
   signUpText: { color: '#E34398' },
-  helpText: { color: 'white', alignSelf: 'center' },
+  helpText: { alignSelf: 'center' },
   help: { flex: 1, alignItems: 'center' }
 });
