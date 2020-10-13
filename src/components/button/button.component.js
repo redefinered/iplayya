5717686afb3d70ff4ecb14d9f57008a1babf6035
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-13 21:10:09 +0800

    change: made button text bold

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import { Button as RNPButton } from 'react-native-paper';
 
 const Button = ({ style, ...otherProps }) => (
-  <RNPButton style={{ paddingVertical: 10, ...style }} {...otherProps}>
+  <RNPButton
+    uppercase={false}
+    labelStyle={{ fontWeight: 'bold' }}
+    style={{ paddingVertical: 10, ...style }}
+    {...otherProps}
+  >
     {otherProps.children}
   </RNPButton>
 );
