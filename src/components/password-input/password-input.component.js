cfb67c584cd1583296a82492f732c699a2f73bbd
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-14 13:51:24 +0800

    new: added hide state icon to password show/hide action

--- src/components/password-input/password-input.component.js
+++ src/components/password-input/password-input.component.js
@@ -21,9 +21,20 @@ const PasswordInput = ({ style, ...otherProps }) => {
       />
       <Pressable
         onPress={() => setShowText(!showText)}
-        style={{ position: 'absolute', right: 10, height: '100%', justifyContent: 'center' }}
+        style={{
+          position: 'absolute',
+          right: 10,
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 40
+        }}
       >
-        <Icon name="eye" size={40} />
+        <Icon
+          name={showText ? 'close' : 'eye'}
+          size={showText ? 25 : 40}
+          style={{ color: 'rgba(255,255,255,0.5)' }}
+        />
       </Pressable>
     </View>
   );
