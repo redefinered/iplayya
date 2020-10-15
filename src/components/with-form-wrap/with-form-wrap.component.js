15f4646a6e15a54a5a47b5a9779167a3c6596f5d
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-15 15:24:47 +0800

    new: built withFormWrap HOC to be used with screens having forms

---
+++ src/components/with-form-wrap/with-form-wrap.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { KeyboardAvoidingView, View, StyleSheet, Platform } from 'react-native';
+import ScreenContainer from 'components/screen-container.component';
+import { useHeaderHeight } from '@react-navigation/stack';
+
+const styles = StyleSheet.create({
+  container: { flex: 1 }
+});
+
+export default function WithFormWrap(WrappedComponent) {
+  const FormWrap = () => {
+    const headerHeight = useHeaderHeight();
+    return (
+      <ScreenContainer>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+          <View style={{ paddingTop: headerHeight }}>
+            <WrappedComponent />
+          </View>
+        </KeyboardAvoidingView>
+      </ScreenContainer>
+    );
+  };
+
+  return FormWrap;
+}
