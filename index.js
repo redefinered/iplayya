8879a4b2f02d1e17c8fe6aff8af2be7e86e7b246
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-15 15:22:02 +0800

    integrated Noto Sans font

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
+import { Provider as PaperProvider, DefaultTheme, configureFonts } from 'react-native-paper';
 import App from './App';
 import { name as appName } from './app.json';
 
 import ReduxContainer from './redux.container';
 
+const fontConfig = {
+  default: {
+    regular: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'normal'
+    },
+    medium: {
+      fontFamily: 'sans-serif-medium',
+      fontWeight: 'normal'
+    },
+    light: {
+      fontFamily: 'sans-serif-light',
+      fontWeight: 'normal'
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin',
+      fontWeight: 'normal'
+    }
+  }
+};
+
 const theme = {
   ...DefaultTheme,
+  // dark: true,
+  fonts: configureFonts(fontConfig),
   roundness: 8,
   colors: {
     ...DefaultTheme.colors,
