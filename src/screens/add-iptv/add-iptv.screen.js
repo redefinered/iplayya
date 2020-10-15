17732f2697d92594733569f0c3d35a6d8a81ff11
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-15 15:25:28 +0800

    change: modified Add IPTV screen to use withFormWrap HOC

--- src/screens/add-iptv/add-iptv.screen.js
+++ src/screens/add-iptv/add-iptv.screen.js
@@ -1,45 +1,47 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { KeyboardAvoidingView, Platform, Pressable, View, ScrollView } from 'react-native';
+import { Pressable, View, ScrollView } from 'react-native';
 import { Text } from 'react-native-paper';
-import ScreenContainer from 'components/screen-container.component';
 import ContentWrap from 'components/content-wrap.component';
 import TextInput from 'components/text-input/text-input.component';
 import PasswordInput from 'components/password-input/password-input.component';
 import Button from 'components/button/button.component';
 import AlertModal from 'components/alert-modal/alert-modal.component';
-import { useHeaderHeight } from '@react-navigation/stack';
+import Loader from 'components/loader.component';
+
+import WithFormWrap from 'components/with-form-wrap/with-form-wrap.component';
 
 import styles from './add-iptv.styles';
 
-const AddIptvScreen = () => {
+const AddIptvScreen = ({ isFething }) => {
   const [modalVisible, setModalVisible] = React.useState(false);
-  const headerHeight = useHeaderHeight();
   return (
-    <ScreenContainer>
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      >
-        <ContentWrap style={{ paddingTop: headerHeight, ...styles.content }}>
-          <ScrollView>
-            <View>
-              <TextInput style={styles.textInput} placeholder="IPTV provider name" />
-              <TextInput style={styles.textInput} placeholder="Portal address" />
-              <TextInput style={styles.textInput} placeholder="Username" />
-              <PasswordInput style={styles.textInput} />
-              <Button style={styles.submit} mode="contained" onPress={() => setModalVisible(true)}>
-                Add IPTV
-              </Button>
-            </View>
-            <Pressable style={styles.skip}>
-              <Text>Skip for now</Text>
-            </Pressable>
-          </ScrollView>
-        </ContentWrap>
-      </KeyboardAvoidingView>
-      <AlertModal showAction={setModalVisible} visible={modalVisible} />
-    </ScreenContainer>
+    <React.Fragment>
+      {isFething && <Loader size="large" />}
+      <ContentWrap style={styles.content}>
+        <ScrollView>
+          <View>
+            <TextInput style={styles.textInput} placeholder="IPTV provider name" />
+            <TextInput style={styles.textInput} placeholder="Portal address" />
+            <TextInput style={styles.textInput} placeholder="Username" />
+            <PasswordInput style={styles.textInput} />
+            <Button style={styles.submit} mode="contained" onPress={() => setModalVisible(true)}>
+              Add IPTV
+            </Button>
+          </View>
+          <Pressable style={styles.skip}>
+            <Text>Skip for now</Text>
+          </Pressable>
+        </ScrollView>
+      </ContentWrap>
+      <AlertModal
+        message="Oops! Your credentials is not valid. Call your IPTV provider for assistance."
+        showAction={setModalVisible}
+        visible={modalVisible}
+      />
+    </React.Fragment>
   );
 };
 
-export default AddIptvScreen;
+export default WithFormWrap(AddIptvScreen);
--- src/screens/add-iptv/add-iptv.styles.js
+++ src/screens/add-iptv/add-iptv.styles.js
@@ -2,10 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: { flex: 1 },
-  content: {
-    flex: 1,
-    justifyContent: 'space-between'
-  },
+  content: { paddingTop: 30 },
   textInput: { backgroundColor: 'rgba(255,255,255,0.1)' },
   submit: { marginBottom: 30, marginTop: 15 },
   skip: { alignItems: 'center', paddingBottom: 50 }
