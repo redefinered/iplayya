538f8519f1edc2d65a71e34f480b8c40a51e0c5e
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-16 18:16:25 +0800

    change: added withFormWrap HOC to sign-up screen

--- src/screens/sign-up/sign-up.screen.js
+++ src/screens/sign-up/sign-up.screen.js
@@ -1,50 +1,38 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+// import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { Text } from 'react-native-paper';
-import ScreenContainer from 'components/screen-container.component';
+// import ScreenContainer from 'components/screen-container.component';
 import TextInput from 'components/text-input/text-input.component';
 import PasswordInput from 'components/password-input/password-input.component';
 import Button from 'components/button/button.component';
 import ContentWrap from 'components/content-wrap.component';
-import { useHeaderHeight } from '@react-navigation/stack';
+
+import withFormWrap from 'components/with-form-wrap/with-form-wrap.component';
 
 import styles from './sign-up.styles';
 
 const SignUpScreen = () => {
-  const headerHeight = useHeaderHeight();
   return (
-    <ScreenContainer>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.container}
-      >
-        <ScrollView>
-          <ContentWrap style={{ ...styles.content, paddingTop: headerHeight }}>
-            <TextInput style={styles.textInput} placeholder="First name" />
-            <TextInput style={styles.textInput} placeholder="Last name" />
-            <TextInput style={styles.textInput} placeholder="Username" />
-            <TextInput style={styles.textInput} placeholder="Email" />
-            <PasswordInput style={styles.textInput} />
-            <Text style={styles.agreement}>
-              By tapping Sign Up, you agree to our{' '}
-              <Text
-                style={styles.agreementLink}
-                onPress={() => console.log('show sign-up component')}
-              >
-                Terms
-              </Text>
-              .
-            </Text>
-            <Button style={styles.submit} mode="contained">
-              Sign Up
-            </Button>
-          </ContentWrap>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </ScreenContainer>
+    <ContentWrap style={styles.content}>
+      <TextInput style={styles.textInput} placeholder="First name" />
+      <TextInput style={styles.textInput} placeholder="Last name" />
+      <TextInput style={styles.textInput} placeholder="Username" />
+      <TextInput style={styles.textInput} placeholder="Email" />
+      <PasswordInput style={styles.textInput} />
+      <Text style={styles.agreement}>
+        By tapping Sign Up, you agree to our{' '}
+        <Text style={styles.agreementLink} onPress={() => console.log('show sign-up component')}>
+          Terms
+        </Text>
+        .
+      </Text>
+      <Button style={styles.submit} mode="contained">
+        Sign Up
+      </Button>
+    </ContentWrap>
   );
 };
 
-export default SignUpScreen;
+export default withFormWrap(SignUpScreen);
