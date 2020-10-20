e941b6db1fcf380ab354a6ed02413fa1211423d1
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-20 13:14:02 +0800

    change: applied image icons to tabbar instead of font icons

--- App.js
+++ App.js
@@ -8,19 +8,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// import { View } from 'react-native';
-// import { Colors, IconButton } from 'react-native-paper';
-import HeaderBackImage from 'components/header-back-image/header-back-image.component';
-// import Icon from 'components/icon/icon.component';
-
 import HomeScreen from 'screens/home/home.screen';
 import SignInScreen from 'screens/sign-in/sign-in.screen';
 import SignUpScreen from 'screens/sign-up/sign-up.screen';
-// import AddIptvScreen from 'screens/add-iptv/add-iptv.screen';
 import ForgotPasswordScreen from 'screens/forgot-password/forgot-password.screen';
 import ResetPasswordScreen from 'screens/reset-password/reset-password.screen';
 import IptvScreen from 'screens/iptv/iptv.screen';
 import AccountScreen from 'screens/account/account.screen';
+
+import HeaderBackImage from 'components/header-back-image/header-back-image.component';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -29,99 +25,21 @@ import { selectCurrentUser } from 'modules/ducks/auth/auth.selectors';
 
 import { headerHeight } from 'common/values';
 
-import TabBarIcon from 'components/tabbar-icon/tabbar-icon.component';
+import HomeIcon from 'images/tab-icons/home.svg';
+import HomeIconActive from 'images/tab-icons/home_active.svg';
+import AccountIcon from 'images/tab-icons/account.svg';
+import AccountIconActive from 'images/tab-icons/account_active.svg';
+import IptvIcon from 'images/tab-icons/iptv.svg';
+import IptvIconActive from 'images/tab-icons/iptv_active.svg';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-
-// const HeaderActions = ({ signOutAction }) => {
-//   return (
-//     <View style={{ flexDirection: 'row' }}>
-// <IconButton
-//   icon="account-circle-outline"
-//   color={Colors.blue500}
-//   size={30}
-//   onPress={() => console.log('Pressed')}
-// />
-//       <IconButton
-//         icon="logout-variant"
-//         color={Colors.red500}
-//         size={30}
-//         onPress={() => signOutAction()}
-//       />
-//       <IconButton
-//         style={{ backgroundColor: 'rgba(255,255,255,0.1)' }}
-//         icon="camera"
-//         color={Colors.white}
-//         size={20}
-//         onPress={() => console.log('Pressed')}
-//       />
-//     </View>
-//   );
-// };
 
 const App = ({ currentUser }) => {
   // simulate logged-in state
   currentUser = { name: 'Red' };
 
   if (!currentUser) return <SignInScreen />;
-  // return (
-  //   <NavigationContainer>
-  //     <Stack.Navigator
-  //       initialRouteName="Home"
-  //       screenOptions={{
-  //         headerTransparent: true,
-  //         headerTintColor: 'white',
-  //         headerBackTitleVisible: false,
-  //         headerBackImage: () => <HeaderBackImage />,
-  //         headerStyle: { height: headerHeight },
-  //         headerTitleStyle: { fontSize: 24 },
-  //         headerTitleContainerStyle: { paddingTop: 30 },
-  //         headerLeftContainerStyle: {
-  //           paddingLeft: 15,
-  //           justifyContent: 'flex-end'
-  //         },
-  //         headerRightContainerStyle: styles.headerRightContainerStyle
-  //       }}
-  //     >
-  //       <Stack.Screen
-  //         name="HomeScreen"
-  //         component={HomeScreen}
-  //         options={{
-  //           // headerRight: () => <HeaderActions signOutAction={handleSignOut} />,
-  //           title: 'iPlayya'
-  //         }}
-  //       />
-  //       <Stack.Screen
-  //         name="AddIptvScreen"
-  //         component={AddIptvScreen}
-  //         options={{ title: 'Add IPTV' }}
-  //       />
-  //       <Stack.Screen
-  //         name="SignUpScreen"
-  //         component={SignUpScreen}
-  //         options={{
-  //           title: 'Sign Up'
-  //           // headerRight: () => (
-  //           //   <View style={styles.headerButtonContainer}>
-  //           //     <Icon name="video-settings" style={{ color: 'white' }} size={16} />
-  //           //   </View>
-  //           // ),
-  //         }}
-  //       />
-  //       <Stack.Screen
-  //         name="ForgotPasswordScreen"
-  //         component={ForgotPasswordScreen}
-  //         options={{ title: 'Forgot Password' }}
-  //       />
-  //       <Stack.Screen
-  //         name="ResetPasswordScreen"
-  //         component={ResetPasswordScreen}
-  //         options={{ title: 'Reset Password' }}
-  //       />
-  //     </Stack.Navigator>
-  //   </NavigationContainer>
-  // );
 
   return (
     <NavigationContainer>
@@ -144,85 +62,123 @@ const App = ({ currentUser }) => {
         }}
       >
         <Tab.Screen
-          name="IptvScreen"
-          component={IptvScreen}
-          options={{ tabBarIcon: () => <TabBarIcon name="iptv" /> }}
-          tabPress={() => console.log('test')}
+          name="IPTV"
+          component={IptvStack}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return focused ? <IptvIconActive /> : <IptvIcon />;
+            }
+          }}
         />
         <Tab.Screen
           name="Home"
           component={HomeStack}
           options={{
             tabBarIcon: ({ focused }) => {
-              console.log({ focused });
-              const name = focused ? 'iplayya' : 'account';
-              // use different image states instead of icon. e.g. iplayya, iplayya-active
-              return <TabBarIcon focused={focused} name={name} />;
+              return focused ? <HomeIconActive /> : <HomeIcon />;
             }
           }}
         />
         <Tab.Screen
-          name="AccountScreen"
-          component={AccountScreen}
-          options={{ tabBarIcon: () => <TabBarIcon name="account" /> }}
+          name="Account"
+          component={AccountStack}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return focused ? <AccountIconActive /> : <AccountIcon />;
+            }
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
-const HomeStack = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerTransparent: true,
-        headerTintColor: 'white',
-        headerBackTitleVisible: false,
-        headerBackImage: () => <HeaderBackImage />,
-        headerStyle: { height: headerHeight },
-        headerTitleStyle: { fontSize: 24 },
-        headerTitleContainerStyle: { paddingTop: 30 },
-        headerLeftContainerStyle: {
-          paddingLeft: 15,
-          justifyContent: 'flex-end'
-        },
-        headerRightContainerStyle: styles.headerRightContainerStyle
+const IptvStack = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerTransparent: true,
+      headerTintColor: 'white',
+      headerBackTitleVisible: false,
+      headerBackImage: () => <HeaderBackImage />,
+      headerStyle: { height: headerHeight },
+      headerTitleStyle: { fontSize: 24 },
+      headerTitleContainerStyle: { paddingTop: 30 },
+      headerLeftContainerStyle: {
+        paddingLeft: 15,
+        justifyContent: 'flex-end'
+      },
+      headerRightContainerStyle: styles.headerRightContainerStyle
+    }}
+  >
+    <Stack.Screen name="IPTV" component={IptvScreen} />
+  </Stack.Navigator>
+);
+
+const HomeStack = () => (
+  <Stack.Navigator
+    initialRouteName="Home"
+    screenOptions={{
+      headerTransparent: true,
+      headerTintColor: 'white',
+      headerBackTitleVisible: false,
+      headerBackImage: () => <HeaderBackImage />,
+      headerStyle: { height: headerHeight },
+      headerTitleStyle: { fontSize: 24 },
+      headerTitleContainerStyle: { paddingTop: 30 },
+      headerLeftContainerStyle: {
+        paddingLeft: 15,
+        justifyContent: 'flex-end'
+      },
+      headerRightContainerStyle: styles.headerRightContainerStyle
+    }}
+  >
+    <Stack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{
+        title: 'iPlayya'
       }}
-    >
-      <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{
-          // headerRight: () => <HeaderActions signOutAction={handleSignOut} />,
-          title: 'iPlayya'
-        }}
-      />
-      <Stack.Screen
-        name="SignUpScreen"
-        component={SignUpScreen}
-        options={{
-          title: 'Sign Up'
-          // headerRight: () => (
-          //   <View style={styles.headerButtonContainer}>
-          //     <Icon name="video-settings" style={{ color: 'white' }} size={16} />
-          //   </View>
-          // ),
-        }}
-      />
-      <Stack.Screen
-        name="ForgotPasswordScreen"
-        component={ForgotPasswordScreen}
-        options={{ title: 'Forgot Password' }}
-      />
-      <Stack.Screen
-        name="ResetPasswordScreen"
-        component={ResetPasswordScreen}
-        options={{ title: 'Reset Password' }}
-      />
-    </Stack.Navigator>
-  );
-};
+    />
+    <Stack.Screen
+      name="SignUpScreen"
+      component={SignUpScreen}
+      options={{
+        title: 'Sign Up'
+      }}
+    />
+    <Stack.Screen
+      name="ForgotPasswordScreen"
+      component={ForgotPasswordScreen}
+      options={{ title: 'Forgot Password' }}
+    />
+    <Stack.Screen
+      name="ResetPasswordScreen"
+      component={ResetPasswordScreen}
+      options={{ title: 'Reset Password' }}
+    />
+  </Stack.Navigator>
+);
+
+const AccountStack = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerTransparent: true,
+      headerTintColor: 'white',
+      headerBackTitleVisible: false,
+      headerBackImage: () => <HeaderBackImage />,
+      headerStyle: { height: headerHeight },
+      headerTitleStyle: { fontSize: 24 },
+      headerTitleContainerStyle: { paddingTop: 30 },
+      headerLeftContainerStyle: {
+        paddingLeft: 15,
+        justifyContent: 'flex-end'
+      },
+      headerRightContainerStyle: styles.headerRightContainerStyle
+    }}
+  >
+    <Stack.Screen name="Account" component={AccountScreen} />
+  </Stack.Navigator>
+);
 
 const mapStateToProps = createStructuredSelector({ currentUser: selectCurrentUser });
 
