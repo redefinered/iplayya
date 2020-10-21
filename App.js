cefc5a97b36e90df891aa253cb73ee5dca62032c
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-21 12:01:54 +0800

    clean up root component

--- App.js
+++ App.js
@@ -5,21 +5,14 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
 
 import SignInScreen from 'screens/sign-in/sign-in.screen';
 import HomeTabs from 'navigators/home-tabs.navigator';
-
-// import HeaderBackImage from 'components/header-back-image/header-back-image.component';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Creators } from 'modules/ducks/auth/auth.actions';
 import { selectCurrentUser } from 'modules/ducks/auth/auth.selectors';
-
-// import { headerHeight } from 'common/values';
-
-// const Stack = createStackNavigator();
 
 const App = ({ currentUser }) => {
   // simulate logged-in state
@@ -35,22 +28,6 @@ const App = ({ currentUser }) => {
 };
 
 const mapStateToProps = createStructuredSelector({ currentUser: selectCurrentUser });
-
-const styles = {
-  headerRightContainerStyle: {
-    paddingRight: 15,
-    justifyContent: 'flex-end'
-  },
-  headerButtonContainer: {
-    width: 44,
-    height: 44,
-    backgroundColor: 'rgba(255,255,255,0.1)',
-    borderRadius: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 15
-  }
-};
 
 const actions = {
   purgeStoreAction: Creators.purgeStore,
