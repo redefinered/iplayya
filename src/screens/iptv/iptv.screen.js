a596c29d768f2e676b97102e67b8f9ffbdd82e45
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-21 10:31:51 +0800

    new: created IPTV screen

--- src/screens/iptv/iptv.screen.js
+++ src/screens/iptv/iptv.screen.js
@@ -1,8 +1,51 @@
 import React from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+import ContentWrap from 'components/content-wrap.component';
+import Spacer from 'components/spacer.component';
+import Button from 'components/button/button.component';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import withHeaderPush from 'components/with-header-push/with-header-push.component';
 
-const IptvScreen = () => {
-  return <Text>ITPV Screen</Text>;
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectProviders } from 'modules/ducks/iptv/iptv.selectors';
+
+import NoProvider from 'images/no_provider.svg';
+
+const IptvScreen = ({ providers }) => {
+  console.log({ providers });
+  if (providers.length)
+    return (
+      <ContentWrap>
+        <Text>ITPV Screen</Text>
+      </ContentWrap>
+    );
+
+  return <NoProviders />;
 };
 
-export default IptvScreen;
+const NoProviders = () => (
+  <View
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingBottom: 130
+    }}
+  >
+    <NoProvider />
+    <Spacer />
+    <Text style={{ fontSize: 24 }}>No providers yet</Text>
+    <Spacer />
+    <Button>Tap to add you IPTV Provider</Button>
+  </View>
+);
+
+IptvScreen.propTypes = {
+  providers: PropTypes.array
+};
+
+const mapStateToProps = createStructuredSelector({ providers: selectProviders });
+
+export default withHeaderPush(connect(mapStateToProps)(IptvScreen));
