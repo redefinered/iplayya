8a6c92cedff25f5a6bef94e3d4b113d4d77be5a8
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-27 09:46:57 +0800

    change: updated contentWrap so it contains a scrollView option

--- src/components/content-wrap.component.js
+++ src/components/content-wrap.component.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
-const ContentWrap = ({ children, style }) => (
-  <View style={{ paddingHorizontal: 15, ...style }}>{children}</View>
-);
+const ContentWrap = ({ children, style, scrollable }) => {
+  if (scrollable) {
+    return <ScrollView style={{ paddingHorizontal: 15, ...style }}>{children}</ScrollView>;
+  }
+  return <View style={{ paddingHorizontal: 15, ...style }}>{children}</View>;
+};
 
 ContentWrap.propTypes = {
   style: PropTypes.object,
-  children: PropTypes.any.isRequired
+  children: PropTypes.any.isRequired,
+  scrollable: PropTypes.bool
 };
 
 export default ContentWrap;
