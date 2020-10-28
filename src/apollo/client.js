185f2b4c1a0d10f103c23fdb367ceeccd2d82cc1
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-28 16:06:12 +0800

    change: fix apollo fetch error because token is not being set

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -8,9 +8,9 @@ const httpLink = createHttpLink({
   uri: 'https://iplayya.herokuapp.com/graphql'
 });
 
-const authLink = setContext((_, { headers }) => {
+const authLink = setContext(async (_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = AsyncStorage.getItem('token');
+  const token = await AsyncStorage.getItem('access_token');
   // return the headers to the context so httpLink can read them
   return {
     headers: {
