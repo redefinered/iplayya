09f333fbe800bed80e67db19100fb788bc574f14
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-11-05 09:46:25 +0800

    code cleanup

--- src/components/with-header-push/with-header-push.component.js
+++ src/components/with-header-push/with-header-push.component.js
@@ -3,19 +3,19 @@ import { View } from 'react-native';
 import ScreenContainer from 'components/screen-container.component';
 import { useHeaderHeight } from '@react-navigation/stack';
 
-export default function withHeaderPush(WrappedComponent, options = {}) {
-  const { backgroundType, gradientTypeColors } = options;
-  // console.log('')
-  const HeaderPush = () => {
+const withHeaderPush = (backgroundType, gradientTypeColors) => (WrappedComponent) => {
+  const headerPush = (props) => {
     const headerHeight = useHeaderHeight();
     return (
       <ScreenContainer backgroundType={backgroundType} gradientTypeColors={gradientTypeColors}>
         <View style={{ flex: 1, paddingTop: headerHeight }}>
-          <WrappedComponent />
+          <WrappedComponent {...props} />
         </View>
       </ScreenContainer>
     );
   };
 
-  return HeaderPush;
-}
+  return headerPush;
+};
+
+export default withHeaderPush;
