595899d26b58e512e153af630e381878bbb8d52f
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-12-09 18:33:58 +0800

    new: imusic library with item multiselect

--- src/screens/imusic/imusic.screen.js
+++ src/screens/imusic/imusic.screen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, Image } from 'react-native';
 import { Text, withTheme } from 'react-native-paper';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import RadioButton from 'components/radio-button/radio-button.component';
 import Icon from 'components/icon/icon.component';
 import Button from 'components/button/button.component';
 import withHeaderPush from 'components/with-header-push/with-header-push.component';
@@ -16,6 +17,45 @@ import { createFontFormat } from 'utils';
 import library from './dummy-data.json';
 
 const ImusicScreen = ({ navigation, theme }) => {
+  const [activateCheckboxes, setActivateCheckboxes] = React.useState(false);
+  const [selectedItems, setSelectedItems] = React.useState([]);
+  const [selectAll, setSellectAll] = React.useState(false);
+
+  const handleSelectItem = (item) => {
+    if (!activateCheckboxes) return;
+
+    const newItems = selectedItems;
+    const index = selectedItems.findIndex((i) => i === item);
+    if (index >= 0) {
+      newItems.splice(index, 1);
+      setSelectedItems([...newItems]);
+    } else {
+      setSelectedItems([item, ...selectedItems]);
+    }
+  };
+
+  React.useEffect(() => {
+    if (selectAll) {
+      let collection = library.map(({ id }) => {
+        return id;
+      });
+      setSelectedItems(collection);
+    } else {
+      setSelectedItems([]);
+    }
+  }, [selectAll]);
+
+  const handleSelectAll = () => {
+    setSellectAll(!selectAll);
+  };
+
+  const handleLongPress = (id) => {
+    setSelectedItems([id]);
+    setActivateCheckboxes(true);
+  };
+
+  console.log({ selectedItems });
+
   return (
     <View style={styles.container}>
       {library.length ? (
@@ -30,6 +70,70 @@ const ImusicScreen = ({ navigation, theme }) => {
               </Text>
             </Pressable>
           </View>
+          <Spacer size={35} />
+
+          {activateCheckboxes && (
+            <React.Fragment>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Icon name="delete" size={24} style={{ marginRight: 10 }} />
+                  <Text style={{ fontWeight: 'bold', ...createFontFormat(12, 16) }}>Delete</Text>
+                </View>
+                <Pressable
+                  onPress={() => handleSelectAll()}
+                  style={{ flexDirection: 'row', alignItems: 'center' }}
+                >
+                  <Text style={{ marginRight: 10 }}>All</Text>
+                  <RadioButton selected={selectedItems.length === library.length} />
+                </Pressable>
+              </View>
+
+              <Spacer size={30} />
+            </React.Fragment>
+          )}
+
+          {library.map(({ id, title, artist, thumbnail }) => (
+            <Pressable
+              key={id}
+              onLongPress={() => handleLongPress(id)}
+              onPress={() => handleSelectItem(id)}
+            >
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginBottom: 20,
+                  justifyContent: 'space-between',
+                  alignItems: 'center'
+                }}
+              >
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Image
+                    style={{ width: 60, height: 60, borderRadius: 8, marginRight: 15 }}
+                    source={{
+                      url: thumbnail
+                    }}
+                  />
+                  <View>
+                    <Text
+                      style={{ fontWeight: 'bold', marginBottom: 5, ...createFontFormat(12, 16) }}
+                    >
+                      {title}
+                    </Text>
+                    <Text style={{ ...createFontFormat(12, 16) }}>{artist}</Text>
+                  </View>
+                </View>
+                {activateCheckboxes && (
+                  <RadioButton selected={selectedItems.findIndex((i) => i === id) >= 0} />
+                )}
+              </View>
+            </Pressable>
+          ))}
         </ContentWrap>
       ) : (
         <View
@@ -56,6 +160,10 @@ const ImusicScreen = ({ navigation, theme }) => {
 
       {/* this element pushes the content so the bottom nav does not go over the content */}
       <View style={{ paddingBottom: 100 }} />
+
+      {/* <View>
+
+      </View> */}
 
       <View
         style={{
