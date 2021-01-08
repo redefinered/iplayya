b97945c870c955265534782909684fb65823223d
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-01-08 10:16:48 +0800

    change: updated profile graphql queries to compensate with API change

--- src/graphql/profile.graphql.js
+++ src/graphql/profile.graphql.js
@@ -38,28 +38,8 @@ export const GET_PROFILE = gql`
 `;
 
 export const UPDATE_PROFILE = gql`
-  mutation UPDATE_PROFILE(
-    $id: ID!
-    $name: String
-    $username: String
-    $first_name: String
-    $last_name: String
-    $email: String
-    $phone: String
-    $birth_date: String
-    $gender: String
-  ) {
-    updateUserProfile(
-      id: $id
-      name: $name
-      username: $username
-      first_name: $first_name
-      last_name: $last_name
-      email: $email
-      phone: $phone
-      birth_date: $birth_date
-      gender: $gender
-    ) {
+  mutation UPDATE_PROFILE($input: updateUserProfileInput) {
+    updateUserProfile(input: $input) {
       id
     }
   }
