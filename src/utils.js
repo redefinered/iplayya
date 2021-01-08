5146d1f918d4eb04ac57130c2b1bc7ef0f759ab2
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-01-09 01:19:58 +0800

    new: added a utility function that generates dates from current day

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export const createFontFormat = (fontSize, lineHeight) => {
   return { fontSize, lineHeight };
 };
@@ -5,4 +7,23 @@ export const createFontFormat = (fontSize, lineHeight) => {
 export const urlEncodeTitle = (title) => {
   const strsplit = title.split(' ');
   return strsplit.join('+');
+};
+
+/**
+ * Generate an array of days starting from now
+ * @param {number} numberDays the number of days to generate
+ */
+export const generateDatesFromToday = (numberDays = 7) => {
+  console.log(numberDays);
+  let dates = [];
+
+  for (let i = 0; i < numberDays; i++) {
+    dates.push({
+      id: i + 1,
+      value: moment().add(i, 'days').valueOf(),
+      formatted: moment().add(i, 'days').format('ddd, MMM D')
+    });
+  }
+
+  return dates;
 };
