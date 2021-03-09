b261f18b2bb8655c667cbbac36975c5093b1f5c8
Red De Guzman <redefinered@gmail.com>
2021-03-09 12:51:18 +0800

    fix: build errors

--- android/app/src/main/java/com/iplayya/MainApplication.java
+++ android/app/src/main/java/com/iplayya/MainApplication.java
@@ -2,14 +2,14 @@ package com.iplayya;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import org.wonday.live.RNLiveStreamPackage;
-import org.wonday.live.RNLiveStreamPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
