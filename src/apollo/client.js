ff040f0980f8cf7db54afeba075fa2eabf63cbfa
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-03-19 19:23:08 +0800

    wip: test secure API

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import AsyncStorage from '@react-native-community/async-storage';
 
-const GRAPHQL_ENDPOINT = 'http://94.130.20.221/graphql';
+const GRAPHQL_ENDPOINT = 'https://94.130.20.221/graphql';
 
 const httpLink = createHttpLink({
   uri: GRAPHQL_ENDPOINT
