f5ead5ca58e9011da07e2c5757dc3deed37107f8
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-04-12 14:49:19 +0800

    change: replaced pressable with touchableOpacity in home screen so there is feedback when selecting an item

--- src/components/home-menu/home-menu.component.js
+++ src/components/home-menu/home-menu.component.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
-import { View, Pressable, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native-paper';
 import Itv from 'assets/btn_itv.svg';
 import Iradio from 'assets/btn_iradio.svg';
@@ -13,42 +13,42 @@ import Isports from 'assets/btn_isports.svg';
 const HomeMenu = ({ navigation }) => {
   return (
     <View style={{ flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center' }}>
-      <Pressable style={styles.menuitem} onPress={() => navigation.replace('ItvScreen')}>
+      <TouchableOpacity style={styles.menuitem} onPress={() => navigation.replace('ItvScreen')}>
         <View style={styles.iconwrap}>
           <Itv />
         </View>
         <Text style={styles.label}>iTV</Text>
-      </Pressable>
-      <Pressable style={styles.menuitem} onPress={() => navigation.replace('ImovieScreen')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuitem} onPress={() => navigation.replace('ImovieScreen')}>
         <View style={styles.iconwrap}>
           <Imovie />
         </View>
         <Text style={styles.label}>iMovie</Text>
-      </Pressable>
-      <Pressable style={styles.menuitem} onPress={() => navigation.replace('IsportsScreen')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuitem} onPress={() => navigation.replace('IsportsScreen')}>
         <View style={styles.iconwrap}>
           <Isports />
         </View>
         <Text style={styles.label}>iSports</Text>
-      </Pressable>
-      <Pressable style={styles.menuitem} onPress={() => navigation.replace('IplayScreen')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuitem} onPress={() => navigation.replace('IplayScreen')}>
         <View style={styles.iconwrap}>
           <Iplay />
         </View>
         <Text style={styles.label}>iPlay</Text>
-      </Pressable>
-      <Pressable style={styles.menuitem} onPress={() => navigation.replace('IradioScreen')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuitem} onPress={() => navigation.replace('IradioScreen')}>
         <View style={styles.iconwrap}>
           <Iradio />
         </View>
         <Text style={styles.label}>iRadio</Text>
-      </Pressable>
-      <Pressable style={styles.menuitem} onPress={() => navigation.replace('ImusicScreen')}>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuitem} onPress={() => navigation.replace('ImusicScreen')}>
         <View style={styles.iconwrap}>
           <Imusic />
         </View>
         <Text style={styles.label}>iMusic</Text>
-      </Pressable>
+      </TouchableOpacity>
     </View>
   );
 };
