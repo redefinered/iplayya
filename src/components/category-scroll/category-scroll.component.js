6f83fd7d5b25782aec6274eb8d56040500b61479
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-06-18 09:46:04 +0800

    change: removed complex code in category scroll component

--- src/components/category-scroll/category-scroll.component.js
+++ src/components/category-scroll/category-scroll.component.js
@@ -10,17 +10,7 @@ import {
 } from 'modules/ducks/movies/movies.selectors';
 import { connect } from 'react-redux';
 
-import dummymusicdata from './dummy-music.json';
-
-const CategoryScroll = ({
-  datatype,
-  category,
-  movies: { videos },
-  onSelect,
-  paginatorOfCategory
-}) => {
-  const [data, setData] = React.useState([]);
-
+const CategoryScroll = ({ category, movies: { videos }, onSelect, paginatorOfCategory }) => {
   if (typeof videos === 'undefined' || videos.length === 0) return <View />;
   let movies = videos.map(({ thumbnail, ...rest }) => {
     return {
@@ -29,11 +19,6 @@ const CategoryScroll = ({
     };
   });
 
-  React.useEffect(() => {
-    if (datatype === 'music') return setData(dummymusicdata);
-    setData(movies);
-  }, []);
-
   // console.log({ data });
   return (
     <View style={{ marginBottom: 30 }}>
@@ -41,8 +26,7 @@ const CategoryScroll = ({
         <Text style={{ fontSize: 16, lineHeight: 22, marginBottom: 15 }}>{category}</Text>
       </ContentWrap>
       <CategoryScrollList
-        data={data}
-        datatype={datatype}
+        data={movies}
         onSelect={onSelect}
         paginatorOfCategory={paginatorOfCategory}
       />
@@ -52,15 +36,10 @@ const CategoryScroll = ({
 
 CategoryScroll.propTypes = {
   id: PropTypes.string,
-  datatype: PropTypes.string,
   movies: PropTypes.object,
   onSelect: PropTypes.func,
   category: PropTypes.string,
   paginatorOfCategory: PropTypes.object
-};
-
-CategoryScroll.defaultProps = {
-  datatype: 'movies'
 };
 
 const mapStateToProps = (state, props) => {
