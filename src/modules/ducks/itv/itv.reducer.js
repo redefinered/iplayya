666f0cd8fb64144b76c48630aabe8f347fa409a4
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-07-21 17:02:29 +0800

    change: converted channel number to Int for orderBy channel number to work

--- src/modules/ducks/itv/itv.reducer.js
+++ src/modules/ducks/itv/itv.reducer.js
@@ -122,7 +122,12 @@ export default createReducer(INITIAL_STATE, {
   [Types.GET_CHANNELS_SUCCESS]: (state, action) => {
     const { channels, nextPaginatorInfo } = action;
 
-    const updatedChannels = uniqBy([...channels, ...state.channels], 'id');
+    let updatedChannels = uniqBy([...channels, ...state.channels], 'id');
+
+    /// convert number property to Int for orderBy function to work
+    updatedChannels = updatedChannels.map(({ number, ...rest }) => {
+      return { number: parseInt(number), ...rest };
+    });
 
     return {
       ...state,
