5a3012d0cc8573f9d025c8bbfcf904b26ef4eb8f
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-08-06 15:15:57 +0800

    change: add the rest of the props to contentWrap returned components

--- src/components/content-wrap.component.js
+++ src/components/content-wrap.component.js
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types';
 import { View, ScrollView } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
-const ContentWrap = ({ children, style, scrollable }) => {
+const ContentWrap = ({ children, style, scrollable, ...rest }) => {
   const theme = useTheme();
   if (scrollable) {
     return (
-      <ScrollView style={{ paddingHorizontal: theme.spacing(2), ...style }}>{children}</ScrollView>
+      <ScrollView style={{ paddingHorizontal: theme.spacing(2), ...style }} {...rest}>
+        {children}
+      </ScrollView>
     );
   }
 
-  return <View style={{ paddingHorizontal: theme.spacing(2), ...style }}>{children}</View>;
+  return (
+    <View style={{ paddingHorizontal: theme.spacing(2), ...style }} {...rest}>
+      {children}
+    </View>
+  );
 };
 
 ContentWrap.propTypes = {
