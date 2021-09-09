99d8dce05478ca8de60bdc0c11bb2cdc3b179876
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-09-09 10:57:58 +0800

    change: added onpress state to next and prev buttons in media player

---
+++ src/components/media-player/next-button.component.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Pressable, StyleSheet } from 'react-native';
+import Icon from 'components/icon/icon.component';
+import theme from 'common/theme';
+
+const NextButton = ({ disabled, onPress }) => {
+  const [isPressed, setIsPressed] = React.useState(false);
+
+  return (
+    <Pressable
+      onPressIn={() => setIsPressed(true)} // replicates TouchableHighlight
+      onPressOut={() => setIsPressed(false)} // replicates TouchableHighlight
+      style={{
+        backgroundColor: isPressed ? theme.iplayya.colors.black80 : 'transparent',
+        opacity: isPressed ? 0.8 : 1,
+        ...styles.buttonContainer
+      }}
+      onPress={() => onPress()}
+      disabled={disabled}
+    >
+      <Icon
+        name="next"
+        size={theme.iconSize(4)}
+        style={{ color: disabled ? theme.iplayya.colors.white25 : 'white' }}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+NextButton.propTypes = {
+  onPress: PropTypes.func,
+  disabled: PropTypes.bool
+};
+
+export default React.memo(NextButton);
