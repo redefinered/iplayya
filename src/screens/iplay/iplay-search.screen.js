cf17ee3709c58fd1fa8e612064ce4b7f7a455e54
Rennand Gonzales <rennand1988@gmail.com>
2021-09-15 16:05:04 +0800

    add: text searchResult

--- src/screens/iplay/iplay-search.screen.js
+++ src/screens/iplay/iplay-search.screen.js
@@ -2,7 +2,8 @@
 
 import React from 'react';
 import { StyleSheet, TextInput as FormInput, View, FlatList } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { useTheme, Text } from 'react-native-paper';
+import { createFontFormat } from 'utils';
 import Icon from 'components/icon/icon.component';
 import ScreenContainer from 'components/screen-container.component';
 import TextInput from 'components/text-input/text-input.component';
@@ -17,7 +18,7 @@ import MediaItem from './media-item.component';
 const IplaySearchScreen = ({ navigation, isFetching, videoFiles }) => {
   const theme = useTheme();
   const [term, setTerm] = React.useState('');
-  const [data, setData] = React.useState();
+  const [data, setData] = React.useState([]);
 
   const handleChange = (value) => {
     setTerm(value);
@@ -25,7 +26,9 @@ const IplaySearchScreen = ({ navigation, isFetching, videoFiles }) => {
 
   React.useEffect(() => {
     if (term.length) {
-      const d = videoFiles.filter(({ name }) => name.search(term) !== -1);
+      const d = videoFiles.filter(
+        ({ name }) => name.toLowerCase().search(term.toLowerCase()) !== -1
+      );
       console.log('ww', d);
       setData(d);
     } else {
@@ -52,24 +55,40 @@ const IplaySearchScreen = ({ navigation, isFetching, videoFiles }) => {
   };
 
   const renderResult = () => {
-    return (
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item: { ...video } }) => {
-          const filesize = video.size / 1e6;
-          return (
-            <MediaItem
-              visible={false}
-              selected={false}
-              filesize={filesize}
-              onSelect={handleItemPress}
-              {...video}
-            />
-          );
-        }}
-      />
-    );
+    if (data.length)
+      return (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={
+            <ContentWrap>
+              <Text
+                style={{
+                  ...createFontFormat(14, 19),
+                  fontWeight: '700',
+                  color: theme.iplayya.colors.white50,
+                  paddingVertical: theme.spacing(2)
+                }}
+              >
+                Search Results
+              </Text>
+            </ContentWrap>
+          }
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item: { ...video } }) => {
+            const filesize = video.size / 1e6;
+            return (
+              <MediaItem
+                visible={false}
+                selected={false}
+                filesize={filesize}
+                onSelect={handleItemPress}
+                {...video}
+              />
+            );
+          }}
+        />
+      );
   };
 
   return (
