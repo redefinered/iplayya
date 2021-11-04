0787f3cfcdf9f544344715f36fd3d3d7540eaaf5
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-11-04 17:14:05 +0800

    change: added side effect to seek player on the current time when selected other resolution in video player

--- src/components/media-player/media-player.component.js
+++ src/components/media-player/media-player.component.js
@@ -28,19 +28,18 @@ const VIDEO_STYLE_FULLSCREEN = {
 };
 
 const MediaPlayer = ({
+  title,
+  source,
+  setSource,
   fullscreen,
   setFullscreen,
-  source,
-  qualitySwitchable,
   thumbnail,
-  title,
   seriesTitle,
   paused,
   togglePlay,
   setPaused,
   multipleMedia,
   videoUrls,
-  setSource,
   previousAction,
   nextAction,
   isFirstEpisode,
@@ -48,6 +47,7 @@ const MediaPlayer = ({
   isSeries,
   currentProgram,
   moduleType,
+  qualitySwitchable,
   // eslint-disable-next-line react/prop-types
   videoLength
 }) => {
@@ -78,7 +78,6 @@ const MediaPlayer = ({
   const [videoStyle, setVideoStyle] = React.useState(VIDEO_STYLE);
   const [showChromecastOptions, setShowChromecastOptions] = React.useState(false);
   // const [chromeCastSession, setChromeCastSession] = React.useState(null);
-  // console.log({ playbackInfo, thumbnail });
 
   const timer = React.useRef(null);
 
@@ -132,6 +131,13 @@ const MediaPlayer = ({
     if (!client) return;
     loadMedia(source);
   }, [source, client]);
+
+  React.useEffect(() => {
+    if (castSessionActive) return;
+    if (!playbackInfo) return;
+
+    player.current.seek(playbackInfo.currentTime);
+  }, [source]);
 
   const getMetadata = () => {
     // const common = {
