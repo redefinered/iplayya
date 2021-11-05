255d3a8b60e4825fc8d5579f6c4e1e9b83a6069f
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-11-05 11:06:07 +0800

    change: applied add to favorites action to like buttons in itv and isports header like buttons

--- src/navigators/home-stack.navigator.js
+++ src/navigators/home-stack.navigator.js
@@ -750,8 +750,8 @@ const HomeStack = ({
                 <View style={{ flexDirection: 'row' }}>
                   <AddToFavoritesButton
                     sub={parseInt(channelId)}
-                    module="itv"
                     active={typeof channel === 'undefined' ? false : channel.is_favorite}
+                    pressAction={rest.addChannelToFavoritesAction}
                   />
                 </View>
               )
@@ -780,7 +780,7 @@ const HomeStack = ({
                   <AddToFavoritesButton
                     sub={parseInt(channelId)}
                     active={typeof channel === 'undefined' ? false : channel.is_favorite}
-                    module="isports"
+                    pressAction={rest.addIsportsChannelToFavoritesAction}
                   />
                 </View>
               )
@@ -831,6 +831,7 @@ const actions = {
   updateDownloadsProgressAction: MoviesCreators.updateDownloadsProgress,
   updateProfileAction: Creators.update,
   addChannelToFavoritesAction: ItvCreators.addToFavorites,
+  addIsportsChannelToFavoritesAction: IsportsCreators.addToFavorites,
   addMovieToFavoritesAction: MoviesCreators.addMovieToFavorites,
   addTrackToFavoritesAction: ImusicFavoritesCreators.addTrackToFavorites,
   addAlbumToFavoritesAction: ImusicFavoritesCreators.addAlbumToFavorites,
