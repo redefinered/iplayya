2175a53ee8eead151ad58d0fc8d945d29f1e1228
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-12-06 11:36:45 +0800

    change: testing notifications

--- App.js
+++ App.js
@@ -64,7 +64,7 @@ const App = ({
   setImusicPaginatorInfoAction
 }) => {
   const [providerError, setProviderError] = React.useState(false);
-  const [testMode, setTestMode] = React.useState(false);
+  const [testMode, setTestMode] = React.useState(true);
   const [notif, setNotif] = React.useState(null);
 
   React.useEffect(() => {
