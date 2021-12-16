b230727e0c9eec4a2079607528641db8ea8c24dd
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-12-16 10:58:03 +0800

    change: added margin top of the player in imovie

--- src/screens/movie-detail/movie-detail.screen.js
+++ src/screens/movie-detail/movie-detail.screen.js
@@ -486,7 +486,10 @@ const MovieDetailScreen = ({
         justifyContent: 'center'
       };
 
-    return {};
+    return {
+      marginTop: fullscreen ? 0 : theme.spacing(2),
+      marginBottom: fullscreen ? 0 : theme.spacing(2)
+    };
   };
 
   /// MAIN
